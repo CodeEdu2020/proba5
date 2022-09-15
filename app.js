1ecafe944e77a099dbee98c74bc300a054d516b0
fonovac <radovan.brajovic@code.edu.rs>
2022-09-15 20:36:53 +0200

    prvi komit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+
+
+    res.send("RADI SVE!!!")
+})
+
+app.listen(PORT, () => {
+
+    console.log("SLUSAM PORT!!!")
+})
